05e2bea16c28f48621534339d44e27f9d3e0788d
Pranav Phatak <pranavphatak99@gmail.com>
2026-09-05 15:59:13 +0530

    Google Search Agent with InMemorySaver

--- Google_Search_Agent.py
+++ Google_Search_Agent.py
@@ -2,17 +2,26 @@ from langchain_tavily import TavilySearch
 from langchain_groq import ChatGroq
 from langchain.agents import create_agent
 from langchain_nvidia_ai_endpoints import ChatNVIDIA
+from langgraph.checkpoint.memory import InMemorySaver
 from dotenv import load_dotenv
 load_dotenv()
 import streamlit as st
 
-model = ChatNVIDIA(model="nvidia/nemotron-3.5-lightning-30b-a3b")
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
 
+if "memory" not in st.session_state:
+    st.session_state.memory = InMemorySaver()
+
+memory = st.session_state.memory
+
+model = ChatNVIDIA(model="nvidia/nemotron-3.5-lightning-30b-a3b")
 search = TavilySearch(max_results=5)
 
 agent = create_agent(
     model = model,
     tools = [search],
+    checkpointer = memory,
     system_prompt = (
         """
 You are a helpful AI search agent.
@@ -34,8 +43,6 @@ st.markdown("Ask questions and I will search the web to find the answer.")
 
 query = st.chat_input("Ask me anything...")
 
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
 
 for message in st.session_state.chat_history:
     role = message["role"]
@@ -51,8 +58,9 @@ if query:
             "messages": [{
                 "role":"user",
                 "content":query
-            }]
-        })
+            }]},
+            {"configurable": {"thread_id": "search_agent_thread"}}
+        )
         answer = response["messages"][-1].content
         st.markdown(answer)
 
