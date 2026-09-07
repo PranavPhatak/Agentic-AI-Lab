e99b37f4c5780fe2865f7e0e6b176f9a661c7b56
Pranav Phatak <pranavphatak99@gmail.com>
2026-09-07 23:03:34 +0530

    learned about document loader

--- rag_document_loader.py
+++ rag_document_loader.py
@@ -1,5 +1,19 @@
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader, PyPDFLoader, CSVLoader, WebBaseLoader
 from dotenv import load_dotenv
 load_dotenv()
 
-loader = TextLoader()
+loader1 = TextLoader("ai.txt")
+docs1 = loader1.load()
+print(docs1[0].page_content, "\n\n\n")
+
+loader2 = PyPDFLoader("ai.pdf")
+docs2 = loader2.load()
+print(docs2[0].page_content, "\n\n\n")
+
+loader3 = CSVLoader("ai.csv")
+docs3 = loader3.load()
+print(docs3, "\n\n\n")
+
+loader4 = WebBaseLoader(web_path="https://docs.langchain.com/oss/python/langchain/streaming")
+docs4 = loader4.load()
+print(docs4[0].page_content)
